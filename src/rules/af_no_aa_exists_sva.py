c15df809c124a408cd4c1ce771526c5d1c61b708
Ajeetha Kumari Venkatesan <ajeethak@asfigo.com>
2025-09-09 04:39:19 +0530

    Enhanced Himank PR with SVA context check

--- src/rules/af_no_aa_exists_sva.py
+++ src/rules/af_no_aa_exists_sva.py
@@ -12,6 +12,20 @@ import anytree
 class AvoidAAExistsSVA(AsFigoLintRule):
     """Checks if Assoc array method exist() is in the SVA code """
 
+    lvMsg = """
+Rule suggested by Ben Cohen:
+
+IEEE LRM 1800 states:
+
+16.6 Boolean expressions: “Elements of dynamic arrays, queues, and associative arrays that are sampled for assertion expression evaluation may get removed from the array or the array may get resized before the assertion expression is evaluated. These specific array elements sampled for assertion expression evaluation shall continue to exist within the scope of the assertion until the assertion expression evaluation completes.”
+
+Many tools are slow to adopt this IEEE 1800 requirement. Several imposed restrictions on the use of associative arrays within assertions, and some prohibited them entirely, likely due to performance concerns.
+
+So avoid usage of array.exists() method call in SVA
+
+e.g.
+((valid, v_err=mem_aarray.exists(addr))
+    """
     def __init__(self, linter):
         self.linter = linter
         self.ruleID = "NO_AA_EXISTS_SVA"
@@ -22,7 +36,9 @@ class AvoidAAExistsSVA(AsFigoLintRule):
                 # check if the node contains "exists"
                 if self.containsExists(hierNode):
                     message = (
-                        f"Debug: Found .exists() in the code. Don't use it\n"
+                        f"{self.lvMsg}"
+                        f"COMPAT: Found .exists() in the code. \n"
+                        f"{curNode.text}"
                     )
                     self.linter.logViolation(self.ruleID, message)
                 
