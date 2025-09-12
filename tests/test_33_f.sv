54abc9856a0ce8fc932250822649af0c610dc9d2
sajanalulu <sajanalulu@gmail.com>
2025-09-12 16:46:38 +0530

    Added tests

--- tests/test_33_f.sv
+++ tests/test_33_f.sv
@@ -2,6 +2,9 @@
    bit go, a, w, y, b, clk;
    initial forever #3 clk = !clk;
    // No detection for 2 occurrence of 'a'
-   ap_1a_within_b: assert property(@ (posedge clk) $rose(go) |=>  (a[=1] within b[->1]));
+   a_1a_within_b: assert property(@ (posedge clk) $rose(go) |=>  (a[=1] within b[->1]));
+   else begin : fail_a_1a_within_b
+    $error("Assertion failed: 'within' operator used (a[=1] within b[->1])");
+  end : fail_a_1a_within_b;
 endmodule : no2_within;
 
